57bef0238cd650236bb80a987209850cc37e5650
edward.ngo <thanh.ngo.2510@gmail.com>
2025-07-14 20:38:48 +0700

    Remove wrapper and enums to avoid adding overhead

--- src/main/java/com/submicro/strukt/art/ArtNode.java
+++ src/main/java/com/submicro/strukt/art/ArtNode.java
@@ -1,14 +1,10 @@
 package com.submicro.strukt.art;
 
-import java.util.Optional;
-
 public interface ArtNode<V> {
 
-    Optional<V> get(byte[] key);
+    V get(long key);
 
-    ArtNode<V> insert(byte[] key, V value);
+    ArtNode<V> insert(long key, V value);
 
-    ArtNode<V> delete(byte[] key);
-
-    ArtNodeType getNodeType();
+    ArtNode<V> delete(long key);
 }
